f34741bbf112a45e01e42b66413bef15caf1d109
Jasbir <mr.jasbirsingh96@gmail.com>
2022-10-19 05:18:54 +0530

    added protectRouteMiddleware

---
+++ middlewares/auth.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+const protectRouteMiddleWare = async (req, res, next) => {
+    try {
+        // you are bringing the token to get your response
+        let decryptedToken = jwt.verify(req.cookies.JWT, process.env.JWT_SECRET);
+        if (decryptedToken) {
+            let userId = decryptedToken.id;
+            //setting userId for other controller function so they can identify user also 
+            req.userId = userId
+            next();
+        } else {
+            res.status(401).json({
+                status: "failure",
+                data: "kindly login to access"
+            });
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: err.message
+        })
+    }
+}
+module.exports={
+    protectRouteMiddleWare
+}
