1f356d05497c60e069e2e62ecbbaeb108b36465c
Jasbir <mr.jasbirsingh96@gmail.com>
2022-10-19 02:12:09 +0530

    created 404 route handler

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require("express");
 
 const app = express();
 
+// added 404 page not found route
+app.use(function (req, res) {
+    res.status(404).json({
+        status: "failure",
+        message: "given route does not exist"
+    })
+})
 
 app.listen(3000,()=>{
     console.log("listening on port 3000")
