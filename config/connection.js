fae97a19dd9a0e295b6e92c47447b0cb78ce40ee
Jasbir <mr.jasbirsingh96@gmail.com>
2022-10-19 03:09:40 +0530

    connecting to database with mysql module using env variables

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+})
+
+connection.connect(function (err,resp){
+    if(err){
+        console.error(err);
+    }else{
+        console.log(resp);
+    }
+});
+module.exports = connection;
