9dda4665a420d556aa8e426849c6ffd40d3defbe
Jasbir <mr.jasbirsingh96@gmail.com>
2022-10-19 03:18:33 +0530

    (node):added express.json

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 
 const app = express();
 require("dotenv").config();
-
+// added to accpet json data of body
+app.use(express.json());
 // added 404 page not found route
 app.use(function (req, res) {
     res.status(404).json({
