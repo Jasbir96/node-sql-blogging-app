e0bdd29702593eeb4d8ccf49f5561eaca4c7a9e6
Jasbir <mr.jasbirsingh96@gmail.com>
2022-11-06 21:43:12 +0530

    (fix) : changed config variable to username

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,14 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection({
     host: process.env.HOST,
-    user: process.env.USER,
+    user: process.env.USERNAME,
     password: process.env.PASSWORD,
     database: process.env.DATABASE
 })
 
 connection.connect(function (err,resp){
     if(err){
+        console.log(process.env.PASSWORD,);
         console.error(err);
     }else{
         console.log(resp);
