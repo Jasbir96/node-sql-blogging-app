01172a21c4ad1cc8e9d0f1ffeb3ce11ef97206ca
Jasbir <mr.jasbirsingh96@gmail.com>
2022-11-06 21:41:21 +0530

    (fix) changed import order to work with env var

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
+const app = express();
+require("dotenv").config();
+console.log(process.env.USER)
 const userRouter = require("./routes/userRoutes");
 const profileRouter = require("./routes/profileRoutes");
 const articleRouter = require("./routes/articleRoutes");
 const { feedController } = require("./controllers/articleController");
-
-const app = express();
-require("dotenv").config();
 // added to accept json data of body
 app.use(express.json());
 
