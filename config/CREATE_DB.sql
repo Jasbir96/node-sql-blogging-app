fc07682edfcd2ec55b57b91e4748682e5556a86b
Jasbir <mr.jasbirsingh96@gmail.com>
2022-11-16 13:03:42 +0530

    (DB):added not null constraint in email for user

--- config/CREATE_DB.sql
+++ config/CREATE_DB.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users(
     id  VARCHAR(100) PRIMARY KEY ,
     username VARCHAR(50) NOT NULL UNIQUE,
-    email VARCHAR(50) UNIQUE,
+    email VARCHAR(50) UNIQUE NOT NULL,
     password_hash VARCHAR(200) NOT NULL,
     bio VARCHAR(255) NOT NULL,
     image VARCHAR(255)
